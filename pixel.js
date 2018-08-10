b87220558efc3b72b34fa5046e0e9827a56f010b
Blythe Totsch <uncblythe1@gmail.com>
2018-08-10 15:16:53 -0500

    event listeners working for pallette and pixels

--- pixel.js
+++ pixel.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () =>{
 console.log('booyah');
-//create main box
+
+//create title
 document.body.innerHTML = `
 <h1> Pixel Art Maker</h1>
  `
-    
-
+//create main box to hold pixel art maker
 const mainBox = document.createElement('div');
 document.body.appendChild(mainBox);
 mainBox.className = 'mainBox';
@@ -16,57 +16,50 @@ mainBox.appendChild(canvas);
 canvas.className = 'canvas';
 console.log(canvas);
 
-//create a pallette w/in main box
-const pallette = document.createElement('div');
-mainBox.appendChild(pallette);
-pallette.className = 'pallette';
+//create a palette w/in main box
+const palette = document.createElement('div');
+mainBox.appendChild(palette);
+palette.className = 'palette';
 
-//create two rows of colors for pallette
+//create two rows of colors for palette
 const rowOne = document.createElement('div');
-pallette.appendChild(rowOne);
+palette.appendChild(rowOne);
 rowOne.className = 'row';
 
-
-
-rowOneColors = ['Snow', 'Azure', 'Beige', 'Bisque', 'BurlyWood', 'DarkOrange', 'FireBrick', 'LightSalmon', 'LightSkyBlue', 'MediumOrchid', 'Plum', 'SeaGreen', 'SlateGrey', 'Yellow']
+// create function for getting colors to assign to the first row of the palette.
+function getColor() {
+    rowOneColors = ['Black', 'Azure', 'Beige', 'Bisque', 'BurlyWood', 'DarkOrange', 'FireBrick', 'LightSalmon', 'LightSkyBlue', 'MediumOrchid', 'Plum', 'SeaGreen', 'SlateGrey', 'Yellow', 'Violet']
     for (let i = 0; i < rowOneColors.length; i ++) {
-        const color = document.createElement('div');
+        let color = document.createElement('div');
         rowOne.appendChild(color);
         color.className = 'pen';
         color.style.backgroundColor =  rowOneColors[i];
+    //add click event to get paintbrush color from pallette
+        color.addEventListener('click', (ev) => {
+            paintBrushColor = color.style.backgroundColor;
+        })
     }
 
-    color.addEventListener('click', (ev) => {
-        console.log('hey');
-    })
+}
 
-
-const rowTwo = document.createElement('div')
-pallette.appendChild(rowTwo);
-rowTwo.className = 'row';
-   
-
-//create divs for each color in the pallette
-
+//makes the paintBrushColor a global variable
+let paintBrushColor = null;
 //create pixels w/in canvas
 function pixelCanvas (){
     for (let i = 0; i < 5000; i ++) {
-        const pixel = document.createElement('div');
+        let pixel = document.createElement('div');
         pixel.className = 'pixel';
         canvas.appendChild(pixel);
 
+        //add paintbrush color to pixel when clicked
         pixel.addEventListener('click', (ev)=> {
-
-        })
+            pixel.style.backgroundColor = paintBrushColor;
+        });
     }
-    //add event listener for coloring individual boxes
+
 }
 
 pixelCanvas();
+getColor();
 
-
-
-
-
-}
-)
+})
